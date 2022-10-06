d5303a864466b7d3270efa55c179adf1c7e3517c
d.saraev <black8.tabls@gmail.com>
2022-10-06 13:41:49 +0300

    init

---
+++ src/test/java/ru/saraev/myspringshop/MySpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.saraev.myspringshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
